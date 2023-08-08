8c5712f75c8ee25bdfe1b1dc67098145e567f3fd
Alex Haimov <alexcool9@gmail.com>
2023-08-08 22:56:08 +0300

    Update Footer.jsx

--- client/src/layout/footer/Footer.jsx
+++ client/src/layout/footer/Footer.jsx
@@ -1,38 +1,55 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-
-
+import { useUser } from "../../users/providers/UserProvider";
+import ROUTES from "../../routes/routesModel";
+import NavItem from "../../routes/NavItem";
 
 const Footer = () => {
+
+  const { user } = useUser();
 
   return (
     <Container>
       <p></p>
       <Grid container spacing={2}>
-        <Grid
+        {/* <Grid
           item
           xs={12}
           md={4}
           sx={{ display: { md: "flex", xs: "none" }, justifyContent: "center" }}
         >
           <h3>Cards</h3>
-        </Grid>
+        </Grid> */}
+        {user?.isBusiness && (
+          <>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              sx={{ display: { md: "flex", xs: "none" }, justifyContent: "center" }}
+            >
+
+              <NavItem to={ROUTES.MY_CARDS} label="My Cards" color="black" />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              sx={{ display: { md: "flex", xs: "none" }, justifyContent: "center" }}
+            >
+              <NavItem to={ROUTES.FAV_CARDS} label="Fav Cards" color="black" />
+            </Grid>
+          </>
+        )}
         <Grid
           item
           xs={12}
           md={4}
           sx={{ display: { md: "flex", xs: "none" }, justifyContent: "center" }}
         >
-          <h3>Nav options</h3>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{ display: { md: "flex", xs: "none" }, justifyContent: "center" }}
-        >
-          <h3>About</h3>
+          <NavItem to={ROUTES.ABOUT} label="About" color="black" />
+
         </Grid>
       </Grid>
       <p></p>
